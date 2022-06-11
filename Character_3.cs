fb85377b1fd70a241b9a9260ef6b480564d926ce
Leo <leo@initiation.com>
2022-06-11 12:36:55 +0200

    Trajectory prediction & velocity instead of Translation

--- Character_3.cs
+++ Character_3.cs
@@ -16,6 +16,7 @@ public class Character_3 : MonoBehaviour
     private float jumpTime = 0;
     private bool isAnchored;
     private Collider2D gravityAnchor;
+    private float gameGravity;
 
     //* ------------------------
     //* Public
@@ -26,10 +27,12 @@ public class Character_3 : MonoBehaviour
     public float JumpTimeLimit;
     public float jumpPower;
     public float gravitationalPull;
+    public float yLimit;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        gameGravity = Mathf.Abs(Physics2D.gravity.y);
     }
 
     void Update()
@@ -37,6 +40,8 @@ public class Character_3 : MonoBehaviour
 
         Translate(Input.GetAxis("Horizontal"));
         RecordJump();
+        PredictCollision();
+
         if (isAnchored && !isJumping)
         {
             PlateformAttraction();
@@ -55,9 +60,20 @@ public class Character_3 : MonoBehaviour
     {
         if (movement != 0)
         {
-            float correctedSpeed = isJumping ? speed / 2 : speed;
+            float correctedSpeed = isAnchored ? speed / 2 : speed;
             // Debug.Log("movement => " + movement);
-            transform.Translate(Vector2.right * movement * Time.deltaTime * correctedSpeed);
+            if (Mathf.Abs(rb.velocity.x) < speed)
+            {
+                rb.velocity += new Vector2(movement /** * Time.deltaTime * correctedSpeed,*/, 0);
+                if (isAnchored)
+                {
+                    transform.Translate(Vector2.right * movement * Time.deltaTime * correctedSpeed);
+                }
+            }
+            else
+            {
+                Debug.Log("Too much velocity");
+            }
             animator.SetBool("isWalking", true);
 
             if (isFacingRight != (movement > 0)) Flip();
@@ -82,7 +98,7 @@ public class Character_3 : MonoBehaviour
 
     private void RecordJump()
     {
-        if (/**isAnchored &&*/ Input.GetButtonDown("Jump"))
+        if (isAnchored && Input.GetButtonDown("Jump"))
         {
             isJumping = true;
             jumpTime = 0;
@@ -124,10 +140,35 @@ public class Character_3 : MonoBehaviour
     private void MatchPlateformFloorAngle(Vector2 collisionPoint)
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, collisionPoint, out hit, 50))
+        if (Physics.Raycast(transform.position, collisionPoint, out hit, 1))
         {
             transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.TransformDirection(Vector3.right), hit.normal));
         }
+    }
+
+
+    //* ------------------------
+    //* Collision detection
+    //* ------------------------
+    private void PredictCollision()
+    {   //! https://www.youtube.com/watch?v=ITsynQy5APg
+        var time = maxTimeY();
+        float x = rb.velocity.x * time;
+        float y = (rb.velocity.y * time) - (gameGravity * Mathf.Pow(time, 2) / 2);
+
+        Vector2 collisionPoint = new Vector2(x + transform.position.x, y + transform.position.y);
+        Debug.DrawLine(transform.position, collisionPoint, Color.green);
+
+        // float x = transform.position.x + rb.velocity.x * Time.fixedDeltaTime;
+        // float y = transform.position.y + rb.velocity.y * Time.fixedDeltaTime - ((-9 * Time.fixedDeltaTime * Time.fixedDeltaTime) / 2);
+        // Debug.DrawLine(transform.position, new Vector2(x, y), Color.green);
+        Debug.DrawLine(transform.position, new Vector2(0, 0), Color.black);
+    }
+
+    private float maxTimeY()
+    {
+        float velocityYPow2 = Mathf.Pow(rb.velocity.y, 2);
+        return (rb.velocity.y + Mathf.Sqrt(velocityYPow2 + 2 * gameGravity * (transform.position.y - -8 /**yLimit*/))) / gameGravity;
     }
 
     //* ------------------------
